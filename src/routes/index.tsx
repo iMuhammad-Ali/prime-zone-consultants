3f30b43195cf9ba01b5be30c80e87126c0134cff
Muhammad Ali <165662784+i-MuhammadAli@users.noreply.github.com>
2025-05-18 09:17:40 +0500

    Added contact us page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "~/layouts/MainLayout";
 import NotFound from "~/pages/404";
+import ContactUs from "~/pages/ContactUs";
 import Home from "~/pages/Home";
 
 export const router = createBrowserRouter([
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "contact-us",
+        element: <ContactUs />,
       },
     ],
   },
